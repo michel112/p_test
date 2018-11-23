cd994f0bb3da073f28e5395db54fd07873e1b3cc
amit <amit.kbizsoft@gmail.com>
2018-11-23 16:49:21 +0530

    new changes

--- main.py
+++ main.py
@@ -1,65 +1,80 @@
 from lxml import etree
-import re
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+
 class Xmlcheck():
-	xml_file = "valid.xml"
-	org_name = "kbiz"
-	brn_name = "test"
+	xml_file = "pom.xml"
+	srch_var = "version"
+	pr_key = "ci_"
+	su_key = "SNAPSHOT"
+	nspace = "http://maven.apache.org/POM/4.0.0"
 	
 	def valid_xml(self):
-		ET.register_namespace('', "http://maven.apache.org/POM/4.0.0")
-		with open(self.xml_file, 'r') as xml_file:
+		xml_files = self.xml_file
+		with open(xml_files, 'r') as xml_file:
 			xml_data = xml_file.read()
 			xmldoc = minidom.parse(self.xml_file)
 			tree = ET.parse(self.xml_file)
 			root = tree.getroot()
-			#get version id
-			item_version = xmldoc.getElementsByTagName("version") 
-			if not item_version:
-				version_vl = ""
-			else:
-				#get version node value
-				version_vl = item_version[0].firstChild.nodeValue
-			#check if snapshot is present in version tag	
-			if 'SNAPSHOT' in version_vl:
-				try:
-					#call check_tag function
-					chk_tag_vl = self.check_tag()
-					print(chk_tag_vl)
-					print("Sucess!!! Valid Xml File")
-					for elem in root.getiterator():
+			item_version = xmldoc.getElementsByTagName(self.srch_var) 
+			#call check_tag function
+			try:
+				etree.fromstring(xml_data)
+				print("Sucess!!! Valid Xml File")
+				#chk snapshot
+				if item_version:
+					#get version node value
+					version_vl = item_version[0].firstChild.nodeValue
+					#check if snapshot is present in version tag	
+					if 'SNAPSHOT' in version_vl:
 						try:
-							elem.text = elem.text.replace(version_vl, chk_tag_vl)
-							tree.write('outputs_main.xml',  xml_declaration = True,encoding = 'utf-8',method = 'xml')
-						except AttributeError:
-							pass
-				except etree.XMLSchemaError:
-					print(XMLSchemaError)
-			else:
-				print("")
-					
+							print("Output string created")
+							#update POM file
+							self.update_xmlVer( version_vl )
+							print("New file generated with name output_main")
+						except etree.XMLSchemaError:
+							print(XMLSchemaError)
+					else:
+						print("SNAPSHOT missing")
+				else:
+					version_vl = ""
+					print("Version missing")
+			except etree.XMLSchemaError:
+							print(XMLSchemaError)		
+
+	#modify xml file
+	def update_xmlVer( self, version_vl ):
+		ET.register_namespace('', self.nspace)
+		xml_files = self.xml_file
+		chk_tag_vl = self.check_tag()
+		tree = ET.parse(self.xml_file)
+		root = tree.getroot()
+		for elem in root.getiterator():
+			try:
+				elem.text = elem.text.replace(version_vl, chk_tag_vl)
+				tree.write(self.xml_file,  xml_declaration = True,encoding = 'utf-8',method = 'xml')
+			except AttributeError:
+				pass
+		
+	
 	def check_tag(self):
 		xmldoc = minidom.parse(self.xml_file)
 		#get tags if present in file
-		item_artifactId = xmldoc.getElementsByTagName("artifactId") 
-		item_groupid = xmldoc.getElementsByTagName("groupId") 
-		valid_vl = "No tag found"
+		branch_name = xmldoc.getElementsByTagName("artifactId") 
+		org_name = xmldoc.getElementsByTagName("groupId") 
 		#check if tag present groupid
-		if not item_groupid:
-			groupid_val = ""
+		if not org_name:
+			org_val = ""
 		else:
-			groupid_val = item_groupid[0].firstChild.nodeValue
+			org_val = org_name[0].firstChild.nodeValue
 		#check if tag present artifactId
-		if not item_artifactId:
-			artifactId_val = ""
+		if not branch_name:
+			branch_val = ""
 		else:
-			artifactId_val = item_artifactId[0].firstChild.nodeValue
+			branch_val = branch_name[0].firstChild.nodeValue
 		
-		val_nm = "ci_"+groupid_val+"_"+artifactId_val+"-SNAPSHOT"
+		val_nm = self.pr_key+org_val+"_"+branch_val+"-"+self.su_key
 		return val_nm
-			
-				
 # create a new object                                            
 ob = Xmlcheck()
 
--- unit_test.py
+++ unit_test.py
@@ -7,50 +7,22 @@ class TestStringMethods(unittest.TestCase):
 	xml_file = "pom.xml"
 	xml_file_valid = "valid.xml"
 	xml_file_invalid = "invalid_pom.xml"
-	org_name = "kbiz"
-	brn_name = "test"
 	
 	def test_valid_xml(self):
-		print("valid")
-		ET.register_namespace('', "http://maven.apache.org/POM/4.0.0")
 		xml_files = self.xml_file
 		xml_inv = self.xml_file_valid
-		tree = ET.parse(self.xml_file_valid)
-		root = tree.getroot()
-		xmldoc = minidom.parse(self.xml_file_valid)
-		item_version = xmldoc.getElementsByTagName("version")
-		if not item_version:
-			version_vl = ""
-		else:
-			#get version node value
-			version_vl = item_version[0].firstChild.nodeValue
+		
 		with open(xml_files, 'r') as xml_file:
 			xml_data = xml_file.read()
 		with open(xml_inv, 'r') as xml_invs:
 			xml_data_inv = xml_invs.read()
 			#call check_tag function
 			chk_tag_vl = self.check_tag()
-			if 'SNAPSHOT' in version_vl:
-				try:
-					#call check_tag function
-					chk_tag_vl = self.check_tag()
-					print(chk_tag_vl)
-					for elem in root.getiterator():
-						try:
-							elem.text = elem.text.replace(version_vl, chk_tag_vl)
-							#create new xml file for result
-							tree.write('output_valid.xml',  xml_declaration = True,encoding = 'utf-8',method = 'xml')
-							#use of Unittest case
-							self.assertEqual(xml_data_inv,xml_data)
-						except AttributeError:
-							pass
-					print("Sucess!!! Valid Xml File")
-				except etree.XMLSchemaError:
-					print(XMLSchemaError)
-				
+			print(chk_tag_vl)
+			if chk_tag_vl == "Valid Tag Value":
+				self.assertEqual(xml_data_inv,xml_data)
 			
 	def test_invalid_xml(self):
-		ET.register_namespace('', "http://maven.apache.org/POM/4.0.0")
 		xml_files = self.xml_file
 		xml_inv = self.xml_file_invalid
 		
@@ -60,41 +32,35 @@ class TestStringMethods(unittest.TestCase):
 			xml_data_inv = xml_invs.read()
 			#call check_tag function
 			chk_tag_vl = self.check_tag()
-			xmldoc = minidom.parse(self.xml_file_valid)
-			#get version by id
-			item_version = xmldoc.getElementsByTagName("version")
-			if not item_version:
-				version_vl = ""
-			else:
-				#get version node value
-				version_vl = item_version[0].firstChild.nodeValue
 			print(chk_tag_vl)
-			if 'SNAPSHOT' in version_vl:
+			if chk_tag_vl == "Valid Tag Value":
 				self.assertEqual(xml_data_inv,xml_data)
-			else:
-				print("Snapshot not found in version tag")
 			
 	def check_tag(self):
+		xml_files = self.xml_file
+		#val_nm = "ci_"+self.org_name+"_"+self.brn_name+"-SNAPSHOT"
 		xmldoc = minidom.parse(self.xml_file)
-		#get tags if present in file
-		item_artifactId = xmldoc.getElementsByTagName("artifactId") 
-		item_groupid = xmldoc.getElementsByTagName("groupId") 
-		valid_vl = "No tag found"
-		#check if tag present groupid
-		if not item_groupid:
-			groupid_val = ""
+		itemlist = xmldoc.getElementsByTagName("version") 
+		if not itemlist:
+			valid_vl = "No tag found"
+			return valid_vl
 		else:
-			groupid_val = item_groupid[0].firstChild.nodeValue
-		#check if tag present artifactId
-		if not item_artifactId:
-			artifactId_val = ""
+			tag_val = itemlist[0].firstChild.nodeValue
+			valid_vl = "tag found"
+			return valid_vl			
+	
+	def test_snap(self):
+		xml_files = self.xml_file
+		#val_nm = "ci_"+self.org_name+"_"+self.brn_name+"-SNAPSHOT"
+		xmldoc = minidom.parse(self.xml_file)
+		item_version = xmldoc.getElementsByTagName("version") 
+		version_vl = item_version[0].firstChild.nodeValue
+		if 'SNAPSHOT' in version_vl:
+			self.assertTrue('SNAPSHOT is present')
 		else:
-			artifactId_val = item_artifactId[0].firstChild.nodeValue
-		
-		val_nm = "ci_"+groupid_val+"_"+artifactId_val+"-SNAPSHOT"
-		return val_nm
-			
-				
+			self.assertFalse('SNAPSHOT not present')
+       
+
 # create a new object                                            
 if __name__ == '__main__':
     unittest.main()
